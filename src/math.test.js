03fd4c2b6e0d32be0524f20a5198685797eac037
eskandari <maedeh.vega1993@gmail.com>
2024-06-05 16:48:57 +0330

    add test for NAN and string

--- src/math.test.js
+++ src/math.test.js
@@ -15,3 +15,18 @@ it ('should summariza all number values in an array',()=>{
     )
     expect(result).toBe(expectedREsult)
 })
+it("should yield NAN if at least one invalid number is provided",()=>{
+    const number=[1,"test"]
+    const result=add(number)
+    expect(result).toBeNaN()
+})
+
+it("should yield correct sum if an array of numeric string values provided",()=>{
+    const number=["1","2"]
+    const result=add(number)
+
+    const expectedREsult=number.reduce(
+        (prevValue,curValue)=>prevValue+ +curValue,0
+    )
+    expect(result).toBe(3)
+})
